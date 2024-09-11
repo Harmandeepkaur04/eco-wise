95528979527a5b855bb61a47e5f19406264773f5
Viclei0928 <141261894+Viclei0928@users.noreply.github.com>
2024-09-11 12:17:41 -0600

    Added Login and Logout
    Used clerk to implement login and logout, added minor styling in homepage.

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { ClerkProvider, SignedIn, SignedOut, SignIn, UserButton } from "@clerk/nextjs";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -19,12 +20,25 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <header className="header-container">
+            <h1 className="header">Eco Wise</h1>
+            <div className="user-button-container">
+            <UserButton showName />
+            </div>
+        </header>
+        <SignedOut>
+          <div className="sign-in-container">
+            <SignIn routing="hash" />
+          </div>
+        </SignedOut>
+        <SignedIn>
+          {children}
+        </SignedIn>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
--- app/page.js
+++ app/page.js
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <p>
-    Eco-wise
-   </p>
+    <main className="mainpage">
+      <div className="info-container">
+        <p>Details here</p>
+        <p>More details</p>
+      </div>
+    </main>
   );
 }
