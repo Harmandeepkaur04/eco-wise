73578dbb50d40d43f5a6b7b9955bdf23d6262bc4
simranjotbal <simranjotkaur.bal@edu.sait.ca>
2024-09-13 15:29:52 -0600

    Home page
    Updated

--- app/disposal/page.tsx
+++ app/disposal/page.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import '../disposal/styles.css';
 import GoogleMaps from '../components/GoogleMaps';
+import Link from 'next/link';
 
 export default function Disposal (){
 
     return <>
+    <main>
+        <header>
+            <nav>
+                <ul>
+                {/* <h1>EcoWise</h1> */}
+                    <li><Link href="/recycle-page">Recycle</Link></li>
+                    <li><Link href="/disposal">Disposal 
+                    </Link></li>
+                    <li><Link href="/rewards-page">Rewards</Link></li>
+                    <li><Link href="/calendar">Calendar</Link></li>
+                    <li><Link href="/profile">Profile</Link></li>
+                </ul> 
+                </nav>    
+        </header>
     <div>
         <h2>Waste Management and Recycling Locations</h2>
     </div>
     <GoogleMaps />
+    </main>
     </>
+    
 
 }
--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
    <main>
     <Index/>
-    <Profile/>
+    
     
    </main>
   );
