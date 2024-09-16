972c3b98028b54926e363a426193f53cf7945f6a
Harmandeep_Kaur <Harmandeep.Kaur04@edu.sait.ca>
2024-09-15 22:31:00 -0600

    Created Rewards page

--- app/rewards-page/page.js
+++ app/rewards-page/page.js
@@ -1,9 +1,29 @@
-export default function RewardsPage(){
+"use client"
+// pages/rewards/index.js
+import Link from 'next/link';
+import Leaderboard from './leaderboard';
+import './styles.css';
 
-    
-    return(
-        <main>
-            Rewards
-        </main>
-    );
-}
+const scores = [
+  { name: 'Alice', points: 10 },
+  { name: 'Bob', points: 7 },
+  { name: 'Charlie', points: 5 },
+];
+
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Recycling Rewards</h1>
+      <div>
+        <Link href="./quiz1">Take Quiz 1</Link>
+      </div>
+      <div>
+        <Link href="quiz2">Take Quiz 2</Link>
+      </div>
+      <Leaderboard scores={scores} />
+    </div>
+  );
+};
+
+export default Home;
