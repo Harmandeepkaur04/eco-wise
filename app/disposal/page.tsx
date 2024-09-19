ba4010d30a35223ea8961dc437e7284a4c4c6c38
Viclei0928 <141261894+Viclei0928@users.noreply.github.com>
2024-09-19 10:10:08 -0600

    Added some information in disposal page

--- app/disposal/page.tsx
+++ app/disposal/page.tsx
@@ -12,20 +12,68 @@ export default function Disposal (){
         <h1>Waste Management and Recycling Locations:</h1>
     </div>
     
-    <div className='div-container'>
-        <h1>East Calgary Landfill and Eco Centre</h1>
-        <br />
-        <h3>Information:</h3>
-        <p>Materials accepted</p>
-        <ul>
-            <li>Residential Waste</li>
-            <li>Commercial Waste</li>
-        </ul>
+    <div className='flex-container'>
+        <div className='div-container'>
+            <h1>East Calgary Landfill and Eco Centre</h1>
+            <p>Materials Accepted:</p>
+            <ul>
+                <li>Residential Waste</li>
+                <li>Commercial Waste</li>
+            </ul>
+            <br />
+            <p>Hours of Operation (April - October):</p>
+            <ul>
+                <li>Monday: 6 am - 5 pm</li>
+                <li>Tuesday: 6 am - 5 pm</li>
+                <li>Wednesday: 6 am - 5 pm</li>
+                <li>Thursday: 6 am - 5 pm</li>
+                <li>Friday: 6 am - 5 pm</li>
+                <li>Saturday: 7:30 am - 5 pm</li>
+                <li>Sunday: 7:30 am - 5 pm</li>
+            </ul>
+        </div>
+
+        <div className='div-container'>
+            <h1>Spyhill Landfill and Eco Centre</h1>
+            <p>Materials Accepted:</p>
+            <ul>
+                <li>Residential Waste</li>
+                <li>Commercial Waste</li>
+            </ul>
+            <br />
+            <p>Hours of Operation (Year Round):</p>
+            <ul>
+                <li>Monday: 7:30 am - 5 pm</li>
+                <li>Tuesday: 7:30 am - 5 pm</li>
+                <li>Wednesday: 7:30 am - 5 pm</li>
+                <li>Thursday: 7:30 am - 5 pm</li>
+                <li>Friday: 7:30 am - 5 pm</li>
+                <li>Saturday: 7:30 am - 5 pm</li>
+                <li>Sunday: CLOSED</li>
+            </ul>
+        </div>
+
+        <div className='div-container'>
+            <h1>Shepard Landfill and Eco Centre</h1>
+            <p>Materials Accepted:</p>
+            <ul>
+                <li>Residential Waste</li>
+                <li>No Commercial Waste except for Clean Fill and Industrial Waste - Permits and Appointments may be required.</li>
+            </ul>
+            <br />
+            <p>Hours of Operation (Year Round):</p>
+            <ul>
+                <li>Monday: 7:30 am - 5 pm</li>
+                <li>Tuesday: 7:30 am - 5 pm</li>
+                <li>Wednesday: 7:30 am - 5 pm</li>
+                <li>Thursday: 7:30 am - 5 pm</li>
+                <li>Friday: 7:30 am - 5 pm</li>
+                <li>Saturday: 7:30 am - 5 pm</li>
+                <li>Sunday: CLOSED</li>
+            </ul>
+        </div>
+        <GoogleMaps />
     </div>
-    
-    <GoogleMaps />
     </main>
     </>
-    
-
 }
