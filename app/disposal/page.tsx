85dc2915d18a74620110025948a9cc06558f1a49
Viclei0928 <141261894+Viclei0928@users.noreply.github.com>
2024-09-27 12:34:54 -0600

    linked div to styling

--- app/disposal/page.tsx
+++ app/disposal/page.tsx
@@ -1,40 +1,48 @@
-import React from 'react';
-import { Button, Text, Table, Container, Paper, Group, Title, Grid } from '@mantine/core';
+import React from "react";
+import {
+  Button,
+  Text,
+  Table,
+  Container,
+  Paper,
+  Group,
+  Title,
+  Grid,
+} from "@mantine/core";
 import "../disposal/styles.css";
 import GoogleMaps from "../components/GoogleMaps";
 
-
-export default function Disposal (){
-
+export default function Disposal() {
   return (
     <Container component="main">
-      <Title component="h2">
-        Waste Management and Recycling Locations
-      </Title>
+      <Title component="h2">Waste Management and Recycling Locations</Title>
 
-            <div className='flex-container'>
-              <Title>East Calgary Landfill and Eco Centre</Title>
-              <Text>Materials Accepted:</Text>
-              <ul>
-                <li>Residential Waste</li>
-                <li>Commercial Waste</li>
-              </ul>
-              <Text>Hours of Operation (April - October):</Text>
-              <ul>
-                <li>Monday: 6 am - 5 pm</li>
-                <li>Tuesday: 6 am - 5 pm</li>
-                <li>Wednesday: 6 am - 5 pm</li>
-                <li>Thursday: 6 am - 5 pm</li>
-                <li>Friday: 6 am - 5 pm</li>
-                <li>Saturday: 7:30 am - 5 pm</li>
-                <li>Sunday: 7:30 am - 5 pm</li>
-              </ul>
+      <div className="flex-container">
+        <div className="locations-container">
+          <div className="div-container">
+            <Title>East Calgary Landfill and Eco Centre</Title>
+            <Text>Materials Accepted:</Text>
+            <ul>
+              <li>Residential Waste</li>
+              <li>Commercial Waste</li>
+            </ul>
+            <Text>Hours of Operation (April - October):</Text>
+            <ul>
+              <li>Monday: 6 am - 5 pm</li>
+              <li>Tuesday: 6 am - 5 pm</li>
+              <li>Wednesday: 6 am - 5 pm</li>
+              <li>Thursday: 6 am - 5 pm</li>
+              <li>Friday: 6 am - 5 pm</li>
+              <li>Saturday: 7:30 am - 5 pm</li>
+              <li>Sunday: 7:30 am - 5 pm</li>
+            </ul>
+          </div>
+        </div>
 
-              <div className="map-container">
-                <GoogleMaps />
-              </div>
-            </div>  
+        <div className="map-container">
+          <GoogleMaps />
+        </div>
+      </div>
     </Container>
-  )
-
+  );
 }
