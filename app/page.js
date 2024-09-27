ff8811989c06b89da47c6d896bb33cc3e6da9c01
Harmandeep_Kaur <Harmandeep.Kaur04@edu.sait.ca>
2024-09-27 15:51:44 -0600

    Adjusted page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Index from "./index/page";
-import pool from "./utils/postgres";
 
 export default function Home() {
   return (
    <main>
     <Index/>
-    
    </main>
   );
 }
