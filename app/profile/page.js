8de02a6ba2933b57873471714079c37fd85443e2
simranjotbal <simranjotkaur.bal@edu.sait.ca>
2024-11-17 13:26:35 -0700

    done2

--- app/profile/page.js
+++ app/profile/page.js
@@ -8,13 +8,7 @@ import { doc, setDoc, getDoc } from 'firebase/firestore';
 import { db } from '../../firebaseConfig';
 import { useAuth } from '@clerk/nextjs';
 import '../profile/styles.css';
-<<<<<<< Updated upstream
 import Invite from '../pages/index'; 
-=======
-
-//import Home from '../pages';
-
->>>>>>> Stashed changes
 
 
 export default function ProfilePage() {
@@ -216,17 +210,14 @@ export default function ProfilePage() {
             alert('Signed out!');
           }}>Sign Out</Button>
         </Group>
-<<<<<<< Updated upstream
 
 
 
         
       </Container>
       
-=======
-      </Container>    
+        
      
->>>>>>> Stashed changes
     </main>
     
   );
